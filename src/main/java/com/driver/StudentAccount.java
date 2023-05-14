b56d9a1202d82f72d637c51ef73101317ede7fbd
USER <9493saikrishna@gmail.com>
2023-05-14 17:54:18 +0530

    please pass

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -1,10 +1,15 @@
 package com.driver;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class StudentAccount extends BankAccount{
 
-    String  institutionName;
+    String institutionName;
 
-    public StudentAccount(String name, double balance, String  institutionName) {
+    public StudentAccount(String name, double balance, String institutionName) {
         //minimum balance is 0 by default
         super(name, balance, 0);
         this.institutionName = institutionName;
